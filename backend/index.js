8d945d8a9d428948c2022516685d3089bb284c00
T-Clement <toquet.clement@gmail.com>
2024-10-14 16:52:54 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const sqlite3 = require("sqlite3").verbose();
+
+const db_name = path.join(__dirname, "data", "apptest.db");
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connexion réussie à la base de données 'apptest.db'");
+});
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+    <nav>
+      <ul>
+        <li><strong>Netfluux</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+      </ul>
+    </nav>
+    <main className="container">
+      <h1>Hello world!</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
